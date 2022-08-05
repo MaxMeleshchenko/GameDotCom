2b45d6a3fef7a6088a9c95d70d71942d984aef87
MaxMeleshchenko <maximkame00@gmail.com>
2022-08-05 13:09:13 +0300

    Initi commit

---
+++ src/GameDotCom/Game.java
@@ -0,0 +1,9 @@
+package GameDotCom;
+
+public class Game {
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
+}
